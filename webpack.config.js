4964e910517f8c323e7f83142b4fd22867c40baa
Marco Antonio Dominguez Rueda <madmzr@gmail.com>
2020-02-28 03:52:40 -0500

    Fixing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -80,24 +80,10 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
-        exclude: /node_modules\/(?!(jedlislider)\/).*/
+        exclude: /node_modules/
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ],
-        exclude: /\.module\.css$/
-      },
-      {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
           'style-loader',
           {
