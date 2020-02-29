57afe53b0ad741f23a0047ce98bcf768021c9a3a
Marco Antonio Dominguez Rueda <madmzr@gmail.com>
2020-02-28 23:07:10 -0500

    Update content

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Typed from 'typed.js';
 import '../style/index.scss';
 
 const intro = document.querySelector('.intro');
+const skills = document.querySelector('.skills');
 
 const typedConfig = {
   typeSpeed: 100,
@@ -12,6 +13,13 @@ const typedConfig = {
   loop: false,
   showCursor: false
 };
+
+function setWindowSize() {
+  intro.style.height = `${Number(window.innerHeight || 0)}px`;
+}
+
+window.addEventListener('DOMContentLoaded', setWindowSize);
+window.addEventListener('resize', setWindowSize);
 
 function authorTitle() {
   // eslint-disable-next-line no-new
@@ -48,3 +56,46 @@ function authorName() {
 }
 
 setTimeout(authorName, 10);
+const fragment = document.createDocumentFragment();
+[
+  'angular.svg',
+  'git.svg',
+  'html-5.svg',
+  'node.svg',
+  'redux-saga.svg',
+  'bootstrap.svg',
+  'github.svg',
+  'imagemin.svg',
+  'postcss.svg',
+  'redux.svg',
+  'css-3.svg',
+  'gitlab.svg',
+  'immutable.svg',
+  'pug.svg',
+  'typescript.svg',
+  'docker.svg',
+  'graphql.svg',
+  'javascript.svg',
+  'react-router.svg',
+  'express.svg',
+  'grunt.svg',
+  'jquery.svg',
+  'react.svg',
+  'foundation.svg',
+  'gulp.svg',
+  'mysql.svg',
+  'sass.svg',
+  'less.svg',
+  'jest.svg',
+  'reactivex.svg'
+].forEach(skill => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = `img/technology/${skill}`;
+  img.className = 'responsive-img';
+  img.loading = 'lazy';
+  li.appendChild(img);
+  fragment.appendChild(li);
+});
+
+skills.appendChild(fragment);
